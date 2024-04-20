c24c8c47126502109e8125cdbab0dee294dba80d
FelipeFioriEmpeltecDesdeCrianca <felipefiori@empeltecjr.com>
2024-04-20 12:31:19 -0300

    Versão 1.1

--- main.py
+++ main.py
@@ -65,4 +65,4 @@ def calcular():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host="0.0.0.0")
